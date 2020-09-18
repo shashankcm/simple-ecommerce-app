c6182e20e88091fa1e99d5d54e66402fea11b0d6
Shashank Gupta <shashankcm90@gmail.com>
2020-09-18 18:30:04 -0400

    Added Top Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import CtaTop from './components/ctaTop/CtaTop'
+
+import './styles/cta-top.css'
 
 function App() {
-  return <div>E-commerce</div>
+  useEffect(() => {
+    /* eslint-disable-next-line */
+    document.title = 'ecommer-app'
+  })
+  return <CtaTop />
 }
 
 export default App
--- src/components/ctaTop/CtaTop.jsx
+++ src/components/ctaTop/CtaTop.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
-const CtaTop = () => <div>CTA Top</div>
+const CtaTop = () => (
+  <div className='cta-top'>
+    <div className='cta-top__img' />
+    <div className='cta-top__textbox'>
+      <div className='cta-top__textbox--headline'>Automate Your Future</div>
+      <div className='cta-top__textbox--story'>
+        Did you know 25% of business owners work for 60 hours per weak? Learn to work smarter, by leveraring
+        technology.
+      </div>
+      <div className='cta-top__textbox--story'>
+        In this 2 week courses, you will focus on the bigger picture while you implement most powerful
+        automated sales system ever created.
+      </div>
+      <input className='cta-top__textbox--button' type='submit' value='Learn the Alchmey of Automation' />
+    </div>
+  </div>
+)
 
 export default CtaTop
