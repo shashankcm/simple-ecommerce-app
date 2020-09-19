fd77b99aba78fdfcfd63d567a7d03ebc1513f4f2
Shashank Gupta <shashankcm90@gmail.com>
2020-09-19 16:36:05 -0400

    Added node-sass media querys for top component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, {useEffect} from 'react'
 import CtaTop from './components/ctaTop/CtaTop'
 
-import './styles/cta-top.css'
-
 function App() {
   useEffect(() => {
     /* eslint-disable-next-line */
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-import './styles/reset.css'
+import './styles/main.css'
 
 ReactDOM.render(
   <React.StrictMode>
